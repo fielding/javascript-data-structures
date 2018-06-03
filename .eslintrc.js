5ba01999fc43422a88554d703efd667edd731ed3
Fielding Johnston <fielding@justfielding.com>
2018-06-03 14:56:15 -0500

    Add various config files

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: 'airbnb',
+  env: {
+    browser: true,
+    node: true,
+    'jest/globals': true,
+  },
+  plugins: ['jest'],
+  rules: {
+    'arrow-parens': [2, 'as-needed'],
+    'no-console': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.mjs'] }],
+    'space-in-parens': 0,
+    'no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }],
+    'react/prop-types': 0,
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'ignore',
+      exports: 'ignore',
+      functions: 'ignore',
+    }],
+  },
+};
